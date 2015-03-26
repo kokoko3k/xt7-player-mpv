78dab842d779db183eaa98e5d9021f56f22d62df
Antonio Orefice <kokoko3k@gmail.com>
2015-03-26 08:40:45 +0100

    Less noisy killer.sh in theory killer.sh should not be needed anymore, because we started to use application_error() killer.sh may be removed in a tuture commit

--- killer.sh
+++ killer.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo 'when $1 dies, $2 will be killed'
+#echo 'when $1 dies, $2 will be killed'
 
 if [ -z "$1" ] ; then exit 0 ; fi
 if [ -z "$2" ] ; then exit 0 ; fi
@@ -15,7 +15,7 @@ mpv_cmd=$(cat /proc/$mpv_pid/cmdline)
 while [ "$xt7_cmd" == "$(cat /proc/$xt7_pid/cmdline)" ] ; do
     #echo xt7 pid $xt7_pid is alive
     if [ "$mpv_cmd" != "$(cat /proc/$mpv_pid/cmdline)" ]; then
-      echo mpv pid $mpv_pid is dead, exiting now
+      #echo mpv pid $mpv_pid is dead, exiting now
       exit 0
     fi
     sleep 10
@@ -23,6 +23,6 @@ done
 
 echo "$1" is dead
 if [ "$mpv_cmd" == "$(cat /proc/$mpv_pid/cmdline)" ]; then
-    echo killing $mpv_pid 
+    echo killing $mpv_pid
     kill $mpv_pid ; sleep 1 ; kill -9 $mpv_pid
 fi
