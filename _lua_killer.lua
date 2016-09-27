fe3b09797d6c092ee93fd5d2cd2583a6abbe24f6
Antonio Orefice <kokoko3k@gmail.com>
2016-09-27 15:28:23 +0200

    comment old code

--- _lua_killer.lua
+++ _lua_killer.lua
@@ -1,7 +1,7 @@
 
 pid=###
 
-timer = mp.add_periodic_timer(0.1, function()
+timer = mp.add_periodic_timer(3, function()
    
    if os.execute( "cd /proc/" .. pid) == nil then
         mp.command("quit")
